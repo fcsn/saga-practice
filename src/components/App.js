bfdf7208c4fdc25d6fa605035853f0c11e4299ea
sirfcsn <sirfcsn@gmail.com>
2018-12-07 13:02:15 +0900

    fix: Fix import path in App.js, Fix loading_spinner.gif

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Button from '../containers/Button';
+import NewsItem from '../containers/NewsItem';
+import Loading from '../containers/Loading';
 
 class App extends Component {
   render() {
     return (
         <div>
-            App
+            <Button />
+            <Loading />
+            <NewsItem />
         </div>
     );
   }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -4,6 +4,10 @@ import { put, takeLatest, all } from 'redux-saga/effects';
 function* fetchNews() {
     const json = yield fetch('https://newsapi.org/v1/articles?source=cnn&apiKey=c39a26d9c12f48dba2a5c00e35684ecc')
         .then(response => response.json() );
+    // put -> redux store에 action을 dispatch한다.
+    // yield ->  제너레이터 함수의 실행을 중지시키거나 그리고  yield 키워드 뒤에오는 표현식[expression]의 값은 제너레이터의 caller로 반환된다.
+    // 제너레이터 버전의 return 키워드로 생각 할 수 있다.
+    // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/yield
     yield put({ type: "NEWS_RECEIVED", json: json.articles });
 }
 function* actionWatcher() {
@@ -11,6 +15,6 @@ function* actionWatcher() {
 }
 export default function* rootSaga() {
     yield all([
-        actionWatcher(),
+        actionWatcher()
     ]);
 }
